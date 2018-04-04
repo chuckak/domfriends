cf230707b410a35f005c3648b8ccfce4ae004ab1
Chuck Aksamit <caksamit@crescendocollective.com>
2018-04-04 11:58:07 -0500

    move code to /src

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { hasClass, addClass, removeClass, toggleClass } from "./class";
-export { css } from "./css";
-export { triggerEvent } from "./event";
-export { getParent } from "./parent";
+export { hasClass, addClass, removeClass, toggleClass } from "./src/class";
+export { css } from "./src/css";
+export { triggerEvent } from "./src/event";
+export { getParent } from "./src/parent";
